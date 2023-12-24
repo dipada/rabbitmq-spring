f8ff1d52fee68c7525904df5838ec2f8fadc567e
dipada <daniele.dipalma68@gmail.com>
2023-12-24 17:59:45 +0100

    added producer model

---
+++ producer/src/main/java/com/example/producer/model/User.java
@@ -0,0 +1,40 @@
+package com.example.producer.model;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import org.springframework.stereotype.Component;
+
+import java.io.Serializable;
+
+@Component
+@JsonIdentityInfo(generator = ObjectIdGenerators.IntSequenceGenerator.class, property = "@id", scope = User.class)
+public class User implements Serializable{
+
+    private String userId;
+    private String username;
+
+
+    public User() {
+    }
+
+    public User(String userId, String username) {
+        this.userId = userId;
+        this.username = username;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
